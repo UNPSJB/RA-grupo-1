0c38d52a6a7c98bc5b06ef45a632ec4bd26eee96
Moraga <lautaromoraga12@gmail.com>
2025-10-26 20:20:20 -0300

    cambios en paneldocente, para que muestre para completar la informacion genera

---
+++ Frontend/src/features/docentes/services/informesService.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8000/informes";
+
+export interface informe{
+    id: number;
+    sede: string;
+    ciclo_lectivo: string;
+    codigo_actividad_curricular: string;
+    docente_responsable: string;
+    cantidad_alumnos_inscriptos: number;
+    cantidad_com_teoricas: number;
+    cantidad_com_practicas: number;
+    estado: string;
+}
+
+export interface InformeCreate extends Omit<Informe, "id"> {}
+
+export const getInformes = async (): Promise<Informe[]> => {
+    const response = await axios.get(API_URL);
+    return response.data;
+}
+
+export const createInforme = async (informe: InformeCreate): Promise<Informe> => {
+    const response = await axios.post(API_URL, informe);
+    return response.data;
+}
+
+export const getInformeById = async (id:number): Promise<Informe> => {
+    const response = await axios.get(`${API_URL}/${id}`);
+    return response.data;
+}
